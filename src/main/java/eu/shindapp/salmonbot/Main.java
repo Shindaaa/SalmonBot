24f3c349c7196ba080566ed5c5f9b20effe89ccf
Shindaaa <shinda.mailpro@gmail.com>
2022-04-08 22:45:02 +0200

    Updating All main modules for SalmonBot

--- src/main/java/eu/shindapp/salmonbot/Main.java
+++ src/main/java/eu/shindapp/salmonbot/Main.java
@@ -1,17 +1,60 @@
 package eu.shindapp.salmonbot;
 
 import eu.shindapp.salmonbot.client.SalmonClient;
+import eu.shindapp.salmonbot.sql.SqlConnection;
+import eu.shindapp.salmonbot.utils.ConfigUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
+import java.sql.Connection;
+
 
 public class Main {
 
+    private static Connection connection;
+    private static SqlConnection sqlConnection;
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+
+        sqlConnection = new SqlConnection(
+                ConfigUtils.getConfig("mysql.urlbase"),
+                ConfigUtils.getConfig("mysql.host"),
+                Integer.parseInt(ConfigUtils.getConfig("mysql.port")),
+                ConfigUtils.getConfig("mysql.database"),
+                ConfigUtils.getConfig("mysql.user"),
+                ConfigUtils.getConfig("mysql.pass")
+        );
+
+        logger.info("Connecting to MySQL Database....");
+
+        try {
+            sqlConnection.connect();
+            logger.info("Connected !");
+        } catch (Exception ex) {
+            logger.error("Unable to connect to Database! Shutting down SalmonBot.");
+            ex.printStackTrace();
+        }
+
+        connection = SqlConnection.getConn();
+
+        logger.info("Connecting to JDA...");
+
         try {
             new SalmonClient().init();
         } catch (LoginException | InterruptedException e) {
+            logger.error("An error occur when trying to connect to JDA.");
             e.printStackTrace();
         }
+
     }
 
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public static SqlConnection getSqlConnection() {
+        return sqlConnection;
+    }
 }
--- src/main/java/eu/shindapp/salmonbot/client/SalmonClient.java
+++ src/main/java/eu/shindapp/salmonbot/client/SalmonClient.java
@@ -1,18 +1,44 @@
 package eu.shindapp.salmonbot.client;
 
-import eu.shindapp.salmonbot.listeners.MessageListener;
+import com.jagrosh.jdautilities.command.CommandClient;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import eu.shindapp.salmonbot.utils.ConfigUtils;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
 
 public class SalmonClient {
 
+    EventWaiter eventWaiter = new EventWaiter();
+
     public void init() throws LoginException, InterruptedException {
 
-        JDABuilder jdaBuilder = JDABuilder.createDefault(ConfigUtils.getConfig("discord_token"));
-        jdaBuilder.addEventListeners(new MessageListener());
-        //... awaiting more parameters
+        CommandClientBuilder builder = new CommandClientBuilder();
+        builder.setOwnerId(ConfigUtils.getConfig("client.ownerId"));
+        builder.setCoOwnerIds(ConfigUtils.getConfig("client.coOwnerId"));
+        builder.setPrefix(ConfigUtils.getConfig("client.prefix"));
+        builder.setAlternativePrefix("<@!" + ConfigUtils.getConfig("client.id") + "> ");
+        builder.setActivity(Activity.watching("play.shindapp.eu"));
+
+        builder.forceGuildOnly(ConfigUtils.getConfig("client.mainGuildId"));
+        builder.addSlashCommands();
+
+        CommandClient client = builder.build();
+
+        JDABuilder jdaBuilder = JDABuilder.createDefault(ConfigUtils.getConfig("client.token"))
+                .addEventListeners(client, eventWaiter)
+                .setChunkingFilter(ChunkingFilter.ALL)
+                .setMemberCachePolicy(MemberCachePolicy.ALL)
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
+                .enableIntents(GatewayIntent.GUILD_PRESENCES)
+                .enableCache(CacheFlag.ACTIVITY);
+
         jdaBuilder.build().awaitReady();
 
     }
